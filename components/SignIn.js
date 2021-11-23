873668f2ad4e1a18bf3e304c7c0675d33d76c113
matthewngid20 <matthew.ng.id20@gmail.com>
2021-11-23 13:34:47 +1100

    added signup's components

--- components/SignIn.js
+++ components/SignIn.js
@@ -55,7 +55,7 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
-        backgroundColor: 'yellow',
+        backgroundColor: '#ffdf3a',
 
     },
     welcomeText: {
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,14 +1,75 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet,Image,TextInput } from 'react-native'
 import { useNavigation } from '@react-navigation/core'
+
+//React elements
+import { Button } from 'react-native-elements';
 
 export function  SignUp (props) {
     const navigation = useNavigation()
     return (
-        <View>
-            <Text> Sign Up</Text>
-            <Button title = "Click here to sign in" onPress = { () => navigation.navigate("SignIn")}/>
+        <View style={styles.container}>
+        <View style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", alignContent: "center", alignItems: "center", marginTop: 64, justifyContent: "center" }}>
+            <Image
+                source={require('../assets/icon.png')}
+            />
+            <Image
+                source={require('../assets/brand.png')}
+            />
         </View>
+        <Text style={styles.welcomeText}> Welcome to EMET</Text>
+        <Text> Email</Text>
+        <TextInput
+            style={styles.textInput}
+            placeholder="User name"
+        />
+        <Text> Password</Text>
+        <TextInput
+            style={styles.textInput}
+            //onChangeText={onChangeNumber}
+            //value={number}
+            placeholder="Password"
+        />
+        <Button
+            title="Create your account"
+            buttonStyle={{ backgroundColor: 'black' }}
+            containerStyle = {{padding: 17}}
+        />
+        <Button
+            title="Click here to login"
+            onPress  = {() => navigation.navigate ("SignIn")}
+            type="clear"
+            containerStyle = {{paddingBottom: 10}}
+        />
+        <Button
+            title="Privacy"    
+            type="clear"            
+        />
+        <Button
+            title="About"    
+            type="clear"            
+        />
+    </View>
     )
 }
+const styles = StyleSheet.create({
+    textInput: {
+        borderWidth: 1,
+        padding: 15,
+        textAlign: 'center',
+        marginBottom: 10,
+
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#ffdf3a',
+
+    },
+    welcomeText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 37,
+    }
+})
 
