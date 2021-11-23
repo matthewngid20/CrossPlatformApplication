7d9f1e75cca2cfc3ab19747db0e71c5f87b85e81
matthewngid20 <matthew.ng.id20@gmail.com>
2021-11-23 12:10:45 +1100

    created config.js and import stack screens

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { SignUp } from './components/SignUp';
+import { SignIn } from './components/SignIn';
+import { Home } from './components/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "SignUp" component ={SignUp}/>
+        <Stack.Screen name = "SignIn" component ={SignIn}/>
+        <Stack.Screen name = "Home" component ={Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import { useNavigation } from '@react-navigation/core'
 
 export function  SignUp (props) {
+    const navigation = useNavigation()
     return (
         <View>
             <Text> Sign Up</Text>
+            <Button title = "Click here to sign in" onPress = { () => navigation.navigate("SignIn")}/>
         </View>
     )
 }
